b0034828a9e1fb786fbc20ae552c45d4f2275ebe
Enrro <edgarbg585@gmail.com>
2017-09-25 21:14:29 -0500

    documentation for later

--- Cracking the conding/Day 10-Binary Numbers/BinaryNumbers.java
+++ Cracking the conding/Day 10-Binary Numbers/BinaryNumbers.java
@@ -5,15 +5,19 @@
  * Given a base-10 integer, n , convert it to binary (base-2). Then find and print the base- integer denoting the maximum
  *  number of consecutive 1's in n's binary representation.
  */
+import java.io.*;
+import java.util.*;
+
 public class BinaryNumbers {
 
-    
-    public static void main(String[] args) {
-        int round = 13;
+    public int consecutiveOnes(int n){
         int max = 0;
         int current = 0;
-        while (0<round) {
-            if (round%2==1) {
+        //divide n until it reaches 0. At each iteration check if there is a remainder
+        // the count the reminders and if there is no reminder the reset the counter.
+        // remember to keep track of the max value everytime you add a reminder. 
+        while (0<n) {
+            if (n%2==1) {
                 current++;
                 if(current>max){
                     max = current;
@@ -22,9 +26,16 @@ public class BinaryNumbers {
             else{
                 current = 0;
             }
-            System.out.println("round: " + round + " current: " + current + " max: " + max);
-            round = round/2;
+            //System.out.println("n: " + n + " current: " + current + " max: " + max);
+            n = n/2;
         }
-
+        return max;
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        BinaryNumbers bN = new BinaryNumbers();
+        System.out.println(bN.consecutiveOnes(n));
     }
 }
