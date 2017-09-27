4fee50eb1e2ff6f2eebba8bafd6cfc1b50cc2a88
Enrro <edgarbg585@gmail.com>
2017-09-26 22:28:49 -0500

    documentation

--- 30days of coding/Day 16-Exceptions - String to Integer/stringToInteger.java
+++ 30days of coding/Day 16-Exceptions - String to Integer/stringToInteger.java
@@ -1,3 +1,8 @@
+/*
+*https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem
+* Not much to say, only useful to remember Integer.parseInt() and NumberFormatException as the exception raised by the 
+* parse if the parse is not fulfilled
+*/
 import java.io.*;
 import java.util.*;
 
